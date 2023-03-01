ba4f77d0cbd940e177722963d0711dc19e381c97
x1aosx <shawsxian@gmail.com>
2023-03-01 17:21:25 +0800

    添加布局

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import App from './App.vue'
 import router from './router/index'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import vue3videoPlay from "vue3-video-play"; // 引入组件
-import "vue3-video-play/dist/style.css"; // 
 
 const app = createApp(App)
 app.use(router)
-app.use(vue3videoPlay)
 app.mount('#app')
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,35 +1,78 @@
 <template>
-    <div>homePage
-        <el-row class="mb-4">
-            <el-button>Default</el-button>
-            <el-button type="primary">Primary</el-button>
-            <el-button type="success">Success</el-button>
-            <el-button type="info">Info</el-button>
-            <el-button type="warning">Warning</el-button>
-            <el-button type="danger">Danger</el-button>
-        </el-row>
-        <el-row>
-            <el-button :icon="Search" circle />
-            <el-button type="primary" :icon="Edit" circle />
-            <el-button type="success" :icon="Check" circle />
-            <el-button type="info" :icon="Message" circle />
-            <el-button type="warning" :icon="Star" circle />
-            <el-button type="danger" :icon="Delete" circle />
-        </el-row>
+    <div class="common-layout">
+        <el-container>
+            <el-aside width="200px">
+                <el-row class="tac" width="200px">
+                    <el-col :span="12">
+                        <h5 class="mb-2">Default colors</h5>
+                        <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
+                            active-text-color="#ffd04b" background-color="#545c64" text-color="#fff">
+                            <el-sub-menu index="1">
+                                <template #title>
+                                    <el-icon>
+                                        <location />
+                                    </el-icon>
+                                    <span>Navigator One</span>
+                                </template>
+                                <el-menu-item-group title="Group One">
+                                    <el-menu-item index="1-1">item one</el-menu-item>
+                                    <el-menu-item index="1-2">item two</el-menu-item>
+                                </el-menu-item-group>
+                                <el-menu-item-group title="Group Two">
+                                    <el-menu-item index="1-3">item three</el-menu-item>
+                                </el-menu-item-group>
+                                <el-sub-menu index="1-4">
+                                    <template #title>item four</template>
+                                    <el-menu-item index="1-4-1">item one</el-menu-item>
+                                </el-sub-menu>
+                            </el-sub-menu>
+                            <el-menu-item index="2">
+                                <el-icon><icon-menu /></el-icon>
+                                <span>Navigator Two</span>
+                            </el-menu-item>
+                            <el-menu-item index="3" disabled>
+                                <el-icon>
+                                    <document />
+                                </el-icon>
+                                <span>Navigator Three</span>
+                            </el-menu-item>
+                            <el-menu-item index="4">
+                                <el-icon>
+                                    <setting />
+                                </el-icon>
+                                <span>Navigator Four</span>
+                            </el-menu-item>
+                        </el-menu>
+                    </el-col>
+                </el-row>
+            </el-aside>
+            <el-container>
+                <el-header>Header</el-header>
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
     </div>
-
 </template>
+  
 
 <script lang="ts" setup>
 import {
-    Check,
-    Delete,
-    Edit,
-    Message,
-    Search,
-    Star,
+    Document,
+    Menu as IconMenu,
+    Location,
+    Setting,
 } from '@element-plus/icons-vue'
+const handleOpen = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath)
+}
 </script>
-<style lang="scss" scoped>
 
+<style scoped>
+.el-aside {
+    background-color: #545c64;
+    height: 100%;
+}
 </style>
